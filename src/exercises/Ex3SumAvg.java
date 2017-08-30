355cb93e19f18a281889376197e5226cac67f112
ostcarl <31474054+ostcarl@users.noreply.github.com>
2017-08-30 16:03:30 +0200

    Revert "Initial commit"
    This reverts commit 5fb855ebe8cbda7213c22af5d4cfd0b4edccfe9e.

--- src/exercises/Ex3SumAvg.java
+++ src/exercises/Ex3SumAvg.java
@@ -17,7 +17,7 @@ public class Ex3SumAvg {
     public static void main(String[] args) {
         new Ex3SumAvg().program();
     }
-//testthisshitdog
+//testthisshitdog2
     final Scanner sc = new Scanner(in);
 
     void program() {
